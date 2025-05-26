0a37362bc4e3b36edd8e2899e81e667dd175bd67
UterGrooll <utergrooll@yandex.ru>
2025-05-26 14:22:52 +0300

    Задание 1.3: Проверка возраста

--- Lesson_1.3.py
+++ Lesson_1.3.py
@@ -1,7 +1,9 @@
 user_input = input("Введите ваш возраст: ")
 
 if user_input.isdigit():
+
     age = int(user_input)
+
     if age < 0:
         print("Ошибка: возраст не может быть отрицательным!")
     elif age >= 18:
