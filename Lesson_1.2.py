2720aae28d6223fd83082c9399890b2c5a5e3f9e
UterGrooll <utergrooll@yandex.ru>
2025-05-26 14:22:10 +0300

    Задание 1.2: Четность числа

--- Lesson_1.2.py
+++ Lesson_1.2.py
@@ -1,7 +1,9 @@
 user_input = input("Введите число: ")
 
 if user_input.isdigit():
+
     number = int(user_input)
+
     if number % 2 == 0:
         print(f"Число {number} является четным")
     else:
