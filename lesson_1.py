a65c1df144c8825cfa4d16a82205ee79e480cb04
UterGrooll <utergrooll@yandex.ru>
2025-05-26 14:19:31 +0300

    Задание 1: Форматирование строк

--- lesson_1.py
+++ lesson_1.py
@@ -1,3 +1,4 @@
+print("Форматирование строк")
 first_name = input("Введите имя: ")
 last_name = input("Введите фамилию: ")
 age = input("Ваш Возраст: ")
@@ -5,7 +6,6 @@ age = input("Ваш Возраст: ")
 output_format = "Ваше имя: {}, Фамилия: {}, Возраст: {} лет".format(first_name, last_name, age)
 print("\nРеализация через format:")
 print(output_format)
-
 output_fstring = f"Ваше имя: {first_name}, Фамилия: {last_name}, Возраст: {age} лет"
 print("\nРеализация через f-string:")
 print(output_fstring)
