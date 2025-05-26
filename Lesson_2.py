cbbc0db7fe261dba97904d77bac2f75927137726
UterGrooll <utergrooll@yandex.ru>
2025-05-26 13:59:35 +0300

    Промежуточный коммит перед rebase

---
+++ Lesson_2.py
@@ -0,0 +1,12 @@
+nums = input("Введите числа через пробел: ").split()
+
+power = int(input("Введите степень: "))
+
+result = []
+for item in nums:
+    if item.lstrip('-').isdigit():
+        result.append(str(int(item) ** power))
+    else:
+        result.append(item * power)
+
+print("Вывод:", " ".join(result))
